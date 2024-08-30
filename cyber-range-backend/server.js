b2b163b5196fc9ca4a53b13ffe21989da240e1c8
john <john.n.sheikh@gmail.com>
2024-08-30 02:40:05 -0400

    adding .env & gitignore

--- cyber-range-backend/server.js
+++ cyber-range-backend/server.js
@@ -19,6 +19,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('Database connection error:', err));
 
+const jwtSecret = process.env.JWT_SECRET
+
 // Routes
 const simulationRoutes = require('./routes/simulationRoutes');
 const authRoutes = require('./routes/authRoutes');
